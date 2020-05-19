df15ca08cb6a08ee53fc5ca4ed21466a4c2fb09a
matenemeth1994 <mate.nemeth@nology.io>
2020-05-19 17:28:48 +0100

    javascript calculator first commit

--- Tech_Test_03/solution/real-trainer-submission/code.mjs
+++ Tech_Test_03/solution/real-trainer-submission/code.mjs
@@ -1,58 +1,56 @@
+import { brandStore } from "./brandStore.mjs";
 
-import { brandStore } from './brandStore.mjs';
-
-import Color from 'color';
+import Color from "color";
 
 const defaultColors = [
-    { name: 'primary', value: '#333' },
-    { name: 'secondary', value: '#222' },
-    { name: 'tertiary', value: '#555' },
+  { name: "primary", value: "#333" },
+  { name: "secondary", value: "#222" },
+  { name: "tertiary", value: "#555" },
 ];
 
-const defaultBodyFont = 'Lato';
-const allowedFonts = ['lato', 'arial', 'helvetica', 'courier'];
+const defaultBodyFont = "Lato";
+const allowedFonts = ["lato", "arial", "helvetica", "courier"];
 
-const convertToRgb = c =>
-    Color(c).rgb().string();
+const convertToRgb = (c) => Color(c).rgb().string();
 
 const validateBodyFont = (bodyFont) => {
-    let found = false;
-    allowedFonts.forEach(a => {
-        if (a === bodyFont.toLowerCase()) found = true;
-    });
-    if (found) return bodyFont;
-    return defaultBodyFont;
-}
+  let found = false;
+  allowedFonts.forEach((a) => {
+    if (a === bodyFont.toLowerCase()) found = true;
+  });
+  if (found) return bodyFont;
+  return defaultBodyFont;
+};
 
 function buildTheme(colors, bodyFont) {
-    if (!colors) colors = defaultColors;
-    if (!bodyFont) colors = defaultBodyFont;
+  if (!colors) colors = defaultColors;
+  if (!bodyFont) colors = defaultBodyFont;
 
-    const newColors = {};
-    colors.forEach(c => {
-        newColors[c.name] = convertToRgb(c.value);
-    });
+  const newColors = {};
+  colors.forEach((c) => {
+    newColors[c.name] = convertToRgb(c.value);
+  });
 
-    return {
-        colors: newColors,
-        bodyFont: validateBodyFont(bodyFont),
-    };
+  return {
+    colors: newColors,
+    bodyFont: validateBodyFont(bodyFont),
+  };
 }
 
 export function getBranding(user) {
-    let colors = defaultColors;
-    let font   = defaultBodyFont
-    if (user.brandId && brandStore.length) {
-            brandStore.some(function(brand) {
-                const brandMatch = (brand.id === user.brandId);
-                if (brandMatch) {
-                    colors = brand.colors;
-                    font   = brand.bodyFont;
-                }
-                return brandMatch
-            });
-        }
-    return buildTheme(colors, font);
+  let colors = defaultColors;
+  let font = defaultBodyFont;
+  if (user.brandId && brandStore.length) {
+    brandStore.some(function (brand) {
+      const brandMatch = brand.id === user.brandId;
+      if (brandMatch) {
+        colors = brand.colors;
+        font = brand.bodyFont;
+      }
+      return brandMatch;
+    });
+  }
+  return buildTheme(colors, font);
 }
 
 //console.log(getBranding({})); // leave this in to show I got existing code working without tests first
